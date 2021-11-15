11c0b1ec7c352565858928e012594905c90e7618
Ellyn Butler <ellynrbutler@gmail.com>
2021-11-15 09:11:13 -0600

    partition added

--- scripts/process/fmriprep_basic.sh
+++ scripts/process/fmriprep_basic.sh
@@ -3,6 +3,7 @@
 #SBATCH --mail-type=FAIL              # Mail events (NONE, BEGIN, END, FAIL, ALL)
 #SBATCH --mail-user=ellynbutler2027@u.northwestern.edu     # Where to send mail
 #SBATCH --ntasks=1                    # Run on a single CPU
+#SBATCH --partition=normal            # Run on a single CPU
 #SBATCH --mem=64gb                    # Job memory request
 #SBATCH --time=30:00:00               # Time limit hrs:min:sec
 #SBATCH --output=serial_test_%j.log   # Standard output and error log
