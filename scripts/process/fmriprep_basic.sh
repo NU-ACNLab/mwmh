3abdb44428932fcfa8e6a7264cebec798456bd18
Ellyn Butler <ellynrbutler@gmail.com>
2021-11-15 12:21:09 -0600

    bind dir

--- scripts/process/fmriprep_basic.sh
+++ scripts/process/fmriprep_basic.sh
@@ -6,15 +6,16 @@
 #SBATCH --ntasks-per-node=1                               ## how many cpus or processors do you need on each computer
 #SBATCH --time=30:10:00                                   ## how long does this need to run (remember different partitions have restrictions on this param)
 #SBATCH --mem-per-cpu=64G                                 ## how much RAM do you need per CPU (this effects your FairShare score so be careful to not ask for more than you need))
-#SBATCH --job-name="sample_job_\${SLURM_ARRAY_TASK_ID}"   ## use the task id in the name of the job
+#SBATCH --job-name="/projects/b1108/data/MWMH/fmriprep_launch/sample_job_\${SLURM_ARRAY_TASK_ID}"   ## use the task id in the name of the job
 #SBATCH --output=sample_job.%A_%a.out                     ## use the jobid (A) and the specific job index (a) to name your log file
 #SBATCH --mail-type=FAIL                                  ## you can receive e-mail alerts from SLURM when your job begins and when your job finishes (completed, failed, etc)
 #SBATCH --mail-user=ellynbutler2027@u.northwestern.edu    ## your email
 
 pwd; hostname; date
 
-singularity run --cleanenv /home/erb9722/fmriprep_20.2.3.sif \
-    /projects/b1108/data/MWMH/bids_directory /projects/b1108/data/MWMH/fmriprep \
+singularity run --cleanenv -B /projects/b1108/data/MWMH \
+    /home/erb9722/fmriprep_20.2.3.sif \
+    /projects/b1108/data/MWMH/bids_directory /projects/b1108/data/MWMH \
     participant \
     --participant-label MWMH001 --fs-license-file /home/erb9722/license.txt \
     --bids-filter-file /projects/b1108/data/MWMH/config/ses-1_config.json
