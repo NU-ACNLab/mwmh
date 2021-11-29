7c713b9f74748e53135a7cc99e68d7a813bb281d
Ellyn Butler <ellynrbutler@gmail.com>
2021-11-29 14:50:58 -0600

    NiftiLabelsMasker now working

--- scripts/process/amygconn.py
+++ scripts/process/amygconn.py
@@ -4,9 +4,11 @@
 ### November 22, 2021
 
 import os
+import json
 import pandas as pd
 import nibabel as nib
-from bids.layout import BIDSLayout #may not be needed
+import numpy as np
+#from bids.layout import BIDSLayout #may not be needed
 from nilearn.input_data import NiftiLabelsMasker
 from nilearn.connectome import ConnectivityMeasure
 from nilearn import plotting
@@ -16,9 +18,9 @@ def main(argv):
    inputfile = ''
    outputfile = ''
    try:
-      opts, args = getopt.getopt(argv,"hi:o:",["idir=","odir=","sub=","ses="])
+      opts, args = getopt.getopt(argv,"hi:o:",["idir=","odir=","sub=","ses="]) # the "hi:o" part may be incomplete
    except getopt.GetoptError:
-      print 'test.py -i <inputdir> -o <outputdir> -s <subject> -ss <session>'
+      print 'amygconn.py -i <inputdir> -o <outputdir> -s <subject> -ss <session>'
       sys.exit(2)
    for opt, arg in opts:
       if opt == '-h':
@@ -70,26 +72,47 @@ confounds_path = os.path.join(funcInDir, [x for x in fList if ('confounds_timese
 confounds_df = pd.read_csv(confounds_path, sep='\t')
 confounds_csv = os.path.join(outDir, subj, ses, 'confounds.csv')
 os.makedirs(os.path.join(outDir, subj, ses), exist_ok=True)
+
+param_file = open(os.path.join(funcInDir, subj+'_'+ses+'_task-rest_run-1_space-MNI152NLin2009cAsym_desc-preproc_bold.json'),)
+param_df = json.load(param_file)
+
+#### Calculate confounds
+# TO DO: Make it what Zach does
+confound_vars = ['trans_x','trans_y','trans_z',
+                 'rot_x','rot_y','rot_z',
+                 'global_signal',
+                 'csf', 'white_matter']
+derivative_columns = ['{}_derivative1'.format(c) for c
+                     in confound_vars]
+final_confounds = confound_vars + derivative_columns
+confounds_df = confounds_df[final_confounds]
+confounds_df = confounds_df.loc[10:] # drop the first 10 TRs from confounds df
 confounds_df.to_csv(confounds_csv, index=False)
 
+#### Remove first 10 TRs
+#https://carpentries-incubator.github.io/SDC-BIDS-fMRI/05-data-cleaning-with-nilearn/index.html
+raw_func_img = nib.load(filefMRI)
+func_img = raw_func_img.slicer[:,:,:,10:]
 
-
-# read docs: detrend, low_pass, high_pass (should depend on TR)
+# read docs: detrend, low_pass, high_pass (should depend on TR?)
 masker = NiftiLabelsMasker(labels_img=labels_img,
                             labels=labels_list,
                             mask_img=mask_img,
                             smoothing_fwhm=6,
-                            standardize=True,
+                            standardize=True, #Check if fMRI does this
                             detrend=True,
-                            low_pass=.01,
-                            high_pass=.08,
-                            verbose=5
+                            low_pass=.08,
+                            high_pass=.01,
+                            verbose=5,
+                            t_r=param_df['RepetitionTime']
                         )
 
-time_series = masker.fit_transform(filefMRI, confounds=confounds_csv)
+time_series = masker.fit_transform(func_img, confounds=confounds_df)
 
 correlation_measure = ConnectivityMeasure(kind='correlation')
 correlation_matrix = correlation_measure.fit_transform([time_series])[0]
+
+# Get row from correlation_matrix that corresponds to the amygdala
 
 # Make a large figure, masking the main diagonal for visualization:
 np.fill_diagonal(correlation_matrix, 0)
