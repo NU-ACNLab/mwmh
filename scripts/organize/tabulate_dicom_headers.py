da7d8037d5bf43598ed406958e2b6e08f7ee444a
Ellyn Butler <ellynrbutler@gmail.com>
2022-02-01 11:55:28 -0600

    tabulate dicom headers from todd

---
+++ scripts/organize/tabulate_dicom_headers.py
@@ -0,0 +1,95 @@
+### This script creates a csv of the various key parameters in the dicom headers
+### for all of the sequences, subjects and sessions
+###
+### Ellyn Butler
+### February 1, 2022
+
+import pydicom #https://github.com/pydicom/pydicom
+# https://pydicom.github.io/pydicom/stable/old/getting_started.html
+import os
+import pandas as pd
+import datetime
+
+todd_dir = '/projects/b1108/todd'
+subject_dirs = os.popen('find '+todd_dir+' -maxdepth 1 -name "MWMH*"').read().split("\n")[:-1]
+#subject_dirs = [sub.replace('\n', '') for sub in subject_dirs]
+
+param_dict = {
+    'subid':[],
+    'sesid':[],
+    'date':[],
+    'Modality':[],
+    'AcquisitionDate':[],
+    'AcquisitionTime':[],
+    'EchoNumbers':[],
+    'EchoTime':[],
+    'FlipAngle':[],
+    'InPlanePhaseEncodingDirection':[],
+    'ProtocolName':[],
+    'RepetitionTime':[],
+    'SequenceName':[],
+    'SliceThickness':[]
+}
+
+for sub_dir in subject_dirs:
+    sequences = os.popen('ls '+sub_dir+'/SCANS/').read().split("\n")[:-1]
+    sequences = [seq for seq in sequences if seq != 'junk']
+    # Skip 99, not a real sequence?
+    sequences = [seq for seq in sequences if '99' not in seq]
+    for seq in sequences:
+        dcm_path = os.popen('find '+sub_dir+'/SCANS/'+seq+'/DICOM/ -name "*.dcm"').read().split("\n")[0]
+        dcm = pydicom.dcmread(dcm_path)
+        sub = sub_dir.split('/')[-1].split('C')[0]
+        ses = sub_dir.split('/')[-1].split('V')[1].split('_')[0]
+        date = sub_dir.split('/')[-1].split('_')
+        if len(date) == 1:
+            date = 'NA'
+        else:
+            date = date[1]
+        param_dict['subid'].append(sub)
+        param_dict['sesid'].append(ses)
+        param_dict['date'].append(ses)
+        if hasattr(dcm, 'AcquisitionDate'):
+            param_dict['AcquisitionDate'].append(dcm.AcquisitionDate)
+        else:
+            param_dict['AcquisitionDate'].append('NA')
+        if hasattr(dcm, 'EchoNumbers'):
+            param_dict['EchoNumbers'].append(dcm.EchoNumbers)
+        else:
+            param_dict['EchoNumbers'].append('NA')
+        if hasattr(dcm, 'EchoTime'):
+            param_dict['EchoTime'].append(dcm.EchoTime)
+        else:
+            param_dict['EchoTime'].append('NA')
+        if hasattr(dcm, 'FlipAngle'):
+            param_dict['FlipAngle'].append(dcm.FlipAngle)
+        else:
+            param_dict['FlipAngle'].append('NA')
+        #param_dict['ImageType'].append(dcm.ImageType)
+        if hasattr(dcm, 'InPlanePhaseEncodingDirection'):
+            param_dict['InPlanePhaseEncodingDirection'].append(dcm.InPlanePhaseEncodingDirection)
+        else:
+            param_dict['InPlanePhaseEncodingDirection'].append('NA')
+        param_dict['Modality'].append(dcm.Modality)
+        param_dict['ProtocolName'].append(dcm.ProtocolName)
+        if hasattr(dcm, 'RepetitionTime'):
+            param_dict['RepetitionTime'].append(dcm.RepetitionTime)
+        else:
+            param_dict['RepetitionTime'].append('NA')
+        if hasattr(dcm, 'SequenceName'):
+            param_dict['SequenceName'].append(dcm.SequenceName)
+        else:
+            param_dict['SequenceName'].append('NA')
+        if hasattr(dcm, 'SliceThickness'):
+            param_dict['SliceThickness'].append(dcm.SliceThickness)
+        else:
+            param_dict['SliceThickness'].append('NA')
+
+
+param_df = pd.DataFrame.from_dict(param_dict)
+param_df.to_csv('/projects/b1108/studies/mwmh/data/raw/meta/params.csv', index=False)
+# ^ Check if there are any duplicate scans here. If so, figure out which one is the
+# best quality and move the others to a junk directory
+
+unique_param_df = param_df.drop('subid', 1).drop('sesid', 1).drop('AcquisitionDate', 1).drop('AcquisitionTime', 1).drop_duplicates()
+unique_param_df.to_csv('/projects/b1108/studies/mwmh/data/raw/meta/unique_params.csv',index=False)
