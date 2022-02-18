8b8e85f5ae1db25ddd6411fc054f63c6020324af
Ellyn Butler <ellynrbutler@gmail.com>
2022-02-17 21:49:44 -0600

    get rid of files argument and replace with dicom dir

--- scripts/curation/launch_tabulate.sh
+++ scripts/curation/launch_tabulate.sh
@@ -6,14 +6,15 @@
 #SBATCH --ntasks-per-node=1                               ## how many cpus or processors do you need on each computer
 #SBATCH --time=30:00:00                                   ## how long does this need to run (remember different partitions have restrictions on this param)
 #SBATCH --mem-per-cpu=100G                                ## how much RAM do you need per CPU (this effects your FairShare score so be careful to not ask for more than you need))
-#SBATCH --job-name="tabulate dicom headers"               ## use the task id in the name of the job
+#SBATCH --job-name="tabulate_dicom_headers"               ## use the task id in the name of the job
 #SBATCH --output=sample_job.%A_%a.out                     ## use the jobid (A) and the specific job index (a) to name your log file
 #SBATCH --mail-type=FAIL                                  ## you can receive e-mail alerts from SLURM when your job begins and when your job finishes (completed, failed, etc)
 #SBATCH --mail-user=ellynbutler2027@u.northwestern.edu    ## your email
 
 singularity run --writable-tmpfs --cleanenv \
   -B /projects/b1108:/base \
-  /home/erb9722/heudiconv_0.9.0.sif --files /base/todd/ \
+  /home/erb9722/heudiconv_0.9.0.sif \
+  --files /base/studies/mwmh/data/raw/neuroimaging/bids/ \
   -s MWMH219 MWMH379 MWMH112 MWMH196 MWMH225 MWMH275 MWMH293 \
   -o /base/studies/mwmh/data/raw/neuroimaging/bids/ -f convertall \
   --grouping all -c none --overwrite
