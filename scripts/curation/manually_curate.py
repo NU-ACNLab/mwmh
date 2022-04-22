272d0bbb621a998d726831dd959e285d5b2004eb
Ellyn Butler <ellynrbutler@gmail.com>
2022-04-22 10:32:41 -0500

    diffusion curated

--- scripts/curation/manually_curate.py
+++ scripts/curation/manually_curate.py
@@ -18,6 +18,18 @@ outdir = '/projects/b1108/studies/mwmh/data/raw/neuroimaging/bids'
 #sub = 'sub-MWMH320'
 #ses = 'ses-1'
 
+def convert_dicoms(sub, ses, dicomdir, bidsdir, modality):
+    converter = Dcm2niix()
+    converter.inputs.source_dir = dicomdir
+    converter.inputs.compression = 5
+    converter.inputs.output_dir = bidsdir+'/'+modality
+    converter.run()
+    json = os.popen('find '+bidsdir+'/'+modality+' -name "*.json"').read().split("\n")[0]
+    os.rename(json, bidsdir+'/'+modality+'/'+sub+'_'+ses+'_'+modality+'.json')
+    nifti = os.popen('find '+bidsdir+'/'+modality+' -name "*.nii.gz"').read().split("\n")[0]
+    os.rename(nifti, bidsdir+'/'+modality+'/'+sub+'_'+ses+'_'+modality+'.nii.gz')
+
+
 def curate_scan(sub, ses, scan, indir):
     dicomdir = indir+'/'+sub+'/'+ses+'/SCANS/'+scan+'/DICOM'
     dcm_path = os.popen('find '+dicomdir+' -name "*.dcm"').read().split("\n")[0]
@@ -29,27 +41,27 @@ def curate_scan(sub, ses, scan, indir):
     # If the ses outdir doesn't exist, make it
     if not os.path.isdir(outdir+'/'+sub+'/'+ses):
         os.mkdir(outdir+'/'+sub+'/'+ses)
-    ssoutdir = outdir+'/'+sub+'/'+ses
+    bidsdir = outdir+'/'+sub+'/'+ses
     if len(dcm_path) > 0:
         dcm = pydicom.dcmread(dcm_path)
         # T1w image
         if (('l_epinav_ME2' in dcm.ProtocolName) or 'MPRAGE_SAG_0.8iso' in dcm.ProtocolName) and (ndicoms == 208) and (dcm.AcquisitionMatrix[1] == 320):
             # If the modality directory doesn't exist, make it
-            if not os.path.isdir(ssoutdir+'/anat'):
-                os.mkdir(ssoutdir+'/anat')
-            converter = Dcm2niix()
-            converter.inputs.source_dir = dicomdir
-            converter.inputs.compression = 5
-            converter.inputs.output_dir = ssoutdir+'/anat'
-            converter.run()
-            # Rename files to be BIDS compliant
-            anatjson = os.popen('find '+ssoutdir+'/anat'+' -name "*.json"').read().split("\n")[0]
-            os.rename(anatjson, ssoutdir+'/anat/'+sub+'_'+ses+'_T1w.json')
-            anatnifti = os.popen('find '+ssoutdir+'/anat'+' -name "*.nii.gz"').read().split("\n")[0]
-            os.rename(anatnifti, ssoutdir+'/anat/'+sub+'_'+ses+'_T1w.nii.gz')
+            modality = 'anat'
+            if not os.path.isdir(bidsdir+'/'+modality):
+                os.mkdir(bidsdir+'/'+modality)
+            convert_dicoms(sub, ses, dicomdir, bidsdir, modality)
         # DTI 9 (10-13 derived)
         elif ('DTI_MB4_68dir_1pt5mm_b1k' == dcm.SeriesDescription) and (ndicoms > 60) and (ndicoms < 70) and (dcm.SliceThickness == 1.5) and (dcm.RepetitionTime == 2500):
-
+            modality = 'dwi'
+            if not os.path.isdir(bidsdir+'/'+modality):
+                os.mkdir(bidsdir+'/'+modality)
+            convert_dicoms(sub, ses, dicomdir, bidsdir, modality)
+            # Rename files to be BIDS compliant
+            bvec = os.popen('find '+bidsdir+'/'+modality+' -name "*.bvec"').read().split("\n")[0]
+            os.rename(bvec, bidsdir+'/'+modality+'/'+sub+'_'+ses+'_'+modality+'.bvec')
+            bval = os.popen('find '+bidsdir+'/'+modality+' -name "*.bval"').read().split("\n")[0]
+            os.rename(bval, bidsdir+'/'+modality+'/'+sub+'_'+ses+'_'+modality+'.bval')
         # faces 16 (15 for 181 1)
         elif (('FACES' in dcm.ProtocolName) or ('MB2_task' in dcm.ProtocolName)) and (ndicoms < 205) and (ndicoms > 195) and (dcm.SliceThickness < 1.8):
 
