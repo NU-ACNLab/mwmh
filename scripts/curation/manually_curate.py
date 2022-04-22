e7b23ae2a2165dea750bab7226ed0dd4803f293e
Ellyn Butler <ellynrbutler@gmail.com>
2022-04-21 23:13:31 -0500

    curation for t1w images working

--- scripts/curation/manually_curate.py
+++ scripts/curation/manually_curate.py
@@ -2,7 +2,7 @@
 ### far too many ways.
 ###
 ### Ellyn Butler
-### April 19, 2022 - April 20, 2022
+### April 19, 2022 - April 21, 2022
 
 import pydicom #https://github.com/pydicom/pydicom
 # https://pydicom.github.io/pydicom/stable/old/getting_started.html
@@ -10,8 +10,10 @@ import os
 import pandas as pd
 import numpy as np
 from datetime import datetime
+from nipype.interfaces.dcm2nii import Dcm2niix
 
 indir = '/projects/b1108/studies/mwmh/data/raw/neuroimaging/dicoms'
+outdir = '/projects/b1108/studies/mwmh/data/raw/neuroimaging/bids'
 
 #sub = 'sub-MWMH320'
 #ses = 'ses-1'
@@ -21,11 +23,30 @@ def curate_scan(sub, ses, scan, indir):
     dcm_path = os.popen('find '+dicomdir+' -name "*.dcm"').read().split("\n")[0]
     dicoms = os.popen('find '+dicomdir+' -name "*.dcm"').read().split("\n")[:-1]
     ndicoms = len(dicoms)
+    # If the sub outdir doesn't exist, make it
+    if not os.path.isdir(outdir+'/'+sub):
+        os.mkdir(outdir+'/'+sub)
+    # If the ses outdir doesn't exist, make it
+    if not os.path.isdir(outdir+'/'+sub+'/'+ses):
+        os.mkdir(outdir+'/'+sub+'/'+ses)
+    ssoutdir = outdir+'/'+sub+'/'+ses
     if len(dcm_path) > 0:
         dcm = pydicom.dcmread(dcm_path)
         # T1w image
-        if (('l_epinav_ME2' in dcm.ProtocolName) | 'MPRAGE_SAG_0.8iso' in dcm.ProtocolName) and (ndicoms == 208) and (dcm.AcquisitionMatrix[1] == 320):
-
+        if (('l_epinav_ME2' in dcm.ProtocolName) or 'MPRAGE_SAG_0.8iso' in dcm.ProtocolName) and (ndicoms == 208) and (dcm.AcquisitionMatrix[1] == 320):
+            # If the modality directory doesn't exist, make it
+            if not os.path.isdir(ssoutdir+'/anat'):
+                os.mkdir(ssoutdir+'/anat')
+            converter = Dcm2niix()
+            converter.inputs.source_dir = dicomdir
+            converter.inputs.compression = 5
+            converter.inputs.output_dir = ssoutdir+'/anat'
+            converter.run()
+            # Rename files to be BIDS compliant
+            anatjson = os.popen('find '+ssoutdir+'/anat'+' -name "*.json"').read().split("\n")[0]
+            os.rename(anatjson, ssoutdir+'/anat/'+sub+'_'+ses+'_T1w.json')
+            anatnifti = os.popen('find '+ssoutdir+'/anat'+' -name "*.nii.gz"').read().split("\n")[0]
+            os.rename(anatnifti, ssoutdir+'/anat/'+sub+'_'+ses+'_T1w.nii.gz')
         # DTI 9 (10-13 derived)
         elif ('DTI_MB4_68dir_1pt5mm_b1k' == dcm.SeriesDescription) and (ndicoms > 60) and (ndicoms < 70) and (dcm.SliceThickness == 1.5) and (dcm.RepetitionTime == 2500):
 
