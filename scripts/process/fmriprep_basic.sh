6d03825f4463567d14b4bff3ce07f2d7acd54b88
Ellyn Butler <ellynrbutler@gmail.com>
2022-04-27 09:40:08 -0500

    fix sbatch

--- scripts/process/fmriprep_basic.sh
+++ scripts/process/fmriprep_basic.sh
@@ -6,7 +6,7 @@
 #SBATCH --ntasks-per-node=1                               ## how many cpus or processors do you need on each computer
 #SBATCH --time=30:00:00                                   ## how long does this need to run (remember different partitions have restrictions on this param)
 #SBATCH --mem-per-cpu=100G                                ## how much RAM do you need per CPU (this effects your FairShare score so be careful to not ask for more than you need))
-#SBATCH --job-name="sample_job_\${SLURM_ARRAY_TASK_ID}"   ## use the task id in the name of the job
+#SBATCH --job-name="fmriprep_test_run"                    ## use the task id in the name of the job
 #SBATCH --output=sample_job.%A_%a.out                     ## use the jobid (A) and the specific job index (a) to name your log file
 #SBATCH --mail-type=FAIL                                  ## you can receive e-mail alerts from SLURM when your job begins and when your job finishes (completed, failed, etc)
 #SBATCH --mail-user=ellynbutler2027@u.northwestern.edu    ## your email
@@ -30,4 +30,4 @@ SINGULARITYENV_TEMPLATEFLOW_HOME=/home/fmriprep/.cache/templateflow \
     --bids-filter-file /projects/b1108/studies/mwmh/config/ses-1_config.json
 
 
-#sbatch -o /projects/b1108/data/MWMH/launch/fmriprep/test.txt /home/erb9722/studies/mwmh/scripts/process/fmriprep_basic.sh
+#sbatch -o /projects/b1108/studies/mwmh/launch/fmriprep/test.txt /projects/b1108/studies/mwmh/scripts/process/fmriprep_basic.sh
