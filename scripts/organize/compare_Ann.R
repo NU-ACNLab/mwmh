69f671b5987b781d63bba73efa6866864c247e17
Ellyn Butler <ellynrbutler@gmail.com>
2022-05-03 09:13:35 -0500

    include missingness output

--- scripts/organize/compare_Ann.R
+++ scripts/organize/compare_Ann.R
@@ -13,5 +13,5 @@ ellyn_subids <- unique(ellyn_df$subid)
 
 # Missing?
 
-ann_subids[!(ann_subids %in% ellyn_subids)]
-ellyn_subids[!(ellyn_subids %in% ann_subids)]
+ann_subids[!(ann_subids %in% ellyn_subids)] #"MWMH129" "MWMH308" "MWMH314" "MWMH001"
+ellyn_subids[!(ellyn_subids %in% ann_subids)] #"MWMH212" "MWMH270"
