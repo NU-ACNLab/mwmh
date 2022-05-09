4a952082264469d355783dc81bea0f6bde151ffc
Ellyn Butler <ellynrbutler@gmail.com>
2022-05-09 12:14:51 -0500

    switch to general

--- scripts/process/launch_fmriprep_21.0.2.py
+++ scripts/process/launch_fmriprep_21.0.2.py
@@ -14,17 +14,17 @@ indir = '/projects/b1108/studies/mwmh/data/raw/neuroimaging/bids/'
 outdir = '/projects/b1108/studies/mwmh/data/processed/neuroimaging/fmriprep/'
 launchdir = '/projects/b1108/studies/mwmh/data/processed/neuroimaging/launch/fmriprep/'
 
-subDirs = glob.glob(indir + "sub-*")
+subdirs = glob.glob(indir + "sub-*")
 
-for subDir in subDirs:
-    subj = subDir.split('/')[6]
-    if not os.path.exists(outdir+subj):
-        os.mkdir(outdir+subj)
-    unprocessed_sessions = np.setdiff1d(os.listdir(indir+subj), os.listdir(outdir+subj))
+for subdir in subdirs:
+    sub = subdir.split('/')[9]
+    if not os.path.exists(outdir+sub):
+        os.mkdir(outdir+sub)
+    unprocessed_sessions = np.setdiff1d(os.listdir(indir+sub), os.listdir(outdir+sub))
     for ses in unprocessed_sessions:
-        if not os.path.exists(outdir+subj+'/'+ses):
-            os.mkdir(outdir+subj+'/'+ses)
-        participant_label = subj.split('-')[1]
+        if not os.path.exists(outdir+sub+'/'+ses):
+            os.mkdir(outdir+sub+'/'+ses)
+        participant_label = sub.split('-')[1]
         cmd = ['SINGULARITYENV_TEMPLATEFLOW_HOME=/home/fmriprep/.cache/templateflow',
             'singularity', 'run', '--writable-tmpfs', '--cleanenv', '--containall',
             '-B /projects/b1108:/projects/b1108',
@@ -37,8 +37,8 @@ for subDir in subDirs:
             '--fs-license-file /opt/freesurfer/license.txt',
             '--output-spaces MNI152NLin6Asym', '--skull-strip-template OASIS30ANTs',
             '--bids-filter-file', '/projects/b1108/studies/mwmh/config/'+ses+'_config.json']
-        fmriprep_script = launchdir+subj+'_'+ses+'_fmriprep_run.sh'
-        os.system('cat /projects/b1108/studies/mwmh/scripts/process/sbatchinfo.sh > '+fmriprep_script)
+        fmriprep_script = launchdir+sub+'_'+ses+'_fmriprep_run.sh'
+        os.system('cat /projects/b1108/studies/mwmh/scripts/process/sbatchinfo_general.sh > '+fmriprep_script)
         os.system('echo '+' '.join(cmd)+' >> '+fmriprep_script)
         os.system('chmod +x '+fmriprep_script)
-        os.system('sbatch -o 'launchdir+subj+'_'+ses+'.txt'+' '+fmriprep_script)
+        os.system('sbatch -o '+launchdir+sub+'_'+ses+'.txt'+' '+fmriprep_script)
