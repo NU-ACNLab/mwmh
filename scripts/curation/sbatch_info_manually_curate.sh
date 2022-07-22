267eb342c75691cdff6081c8606378cdb36ef830
Ellyn Butler <ellynrbutler@gmail.com>
2022-07-21 21:17:40 -0500

    dont need so much memory

--- scripts/curation/sbatch_info_manually_curate.sh
+++ scripts/curation/sbatch_info_manually_curate.sh
@@ -5,7 +5,7 @@
 #SBATCH --nodes=1                                         ## how many computers do you need
 #SBATCH --ntasks-per-node=1                               ## how many cpus or processors do you need on each computer
 #SBATCH --time=00:20:00                                   ## how long does this need to run (remember different partitions have restrictions on this param)
-#SBATCH --mem-per-cpu=10G                                 ## how much RAM do you need per CPU (this effects your FairShare score so be careful to not ask for more than you need))
+#SBATCH --mem-per-cpu=1G                                  ## how much RAM do you need per CPU (this effects your FairShare score so be careful to not ask for more than you need))
 #SBATCH --job-name="manually_curate"                      ## use the task id in the name of the job
 #SBATCH --output=sample_job.%A_%a.out                     ## use the jobid (A) and the specific job index (a) to name your log file
 #SBATCH --mail-type=FAIL                                  ## you can receive e-mail alerts from SLURM when your job begins and when your job finishes (completed, failed, etc)
