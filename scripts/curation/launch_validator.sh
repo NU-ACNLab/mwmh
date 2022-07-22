9cb64e26d7dbf4c80f7bcaed0a0f55a370ccb2ce
Ellyn Butler <ellynrbutler@gmail.com>
2022-07-21 21:22:16 -0500

    less memory for validator

--- scripts/curation/launch_validator.sh
+++ scripts/curation/launch_validator.sh
@@ -5,7 +5,7 @@
 #SBATCH --nodes=1                                         ## how many computers do you need
 #SBATCH --ntasks-per-node=1                               ## how many cpus or processors do you need on each computer
 #SBATCH --time=05:00:00                                   ## how long does this need to run (remember different partitions have restrictions on this param)
-#SBATCH --mem-per-cpu=100G                                ## how much RAM do you need per CPU (this effects your FairShare score so be careful to not ask for more than you need))
+#SBATCH --mem-per-cpu=10G                                 ## how much RAM do you need per CPU (this effects your FairShare score so be careful to not ask for more than you need))
 #SBATCH --job-name="validate bids"                        ## use the task id in the name of the job
 #SBATCH --output=sample_job.%A_%a.out                     ## use the jobid (A) and the specific job index (a) to name your log file
 #SBATCH --mail-type=FAIL                                  ## you can receive e-mail alerts from SLURM when your job begins and when your job finishes (completed, failed, etc)
@@ -17,5 +17,5 @@ singularity exec --writable-tmpfs --cleanenv \
   bids-validator /projects/b1108/studies/mwmh/data/raw/neuroimaging/bids
 
 #MWMH219 MWMH379 MWMH112 MWMH196 MWMH225 MWMH275 MWMH293
-# sbatch -o /projects/b1108/studies/mwmh/data/raw/neuroimaging/bids/tabulate.txt \
+# sbatch -o /projects/b1108/studies/mwmh/launch/validate/tabulate.txt \
 # /projects/b1108/studies/mwmh/scripts/curation/launch_tabulate.sh
