7b0b0196da1c4fcaf73233f6d725ac76ec888da5
Ellyn Butler <ellynrbutler@gmail.com>
2022-07-31 20:55:06 -0500

    test motion calc locally

--- scripts/process/FDcalc_FMRIPREP.m
+++ scripts/process/FDcalc_FMRIPREP.m
@@ -18,7 +18,7 @@ function FDcalc_FMRIPREP(datafile,varargin)
 df = readtable(datafile); %reads into a table structure, with datafile top row as labels
 numdatas=size(df.sub,1); %number of datasets to analyses (subs X sessions)
 
-    
+
 %%% FD/filtering parameters (these will probably be constant within a study but potentially vary across studies)
 if nargin == 1
     % if FD/filtering parameters are not provided, use these defaults
@@ -45,16 +45,16 @@ else
     error('incorrect number of inputs');
 end
 
-    
-              
+
+
 % fmriprep relevant field names from confounds file
-%   order in most sensible order you like 
+%   order in most sensible order you like
 %   n.b: rotation parameters are in radians (from FSL mcflirt)
 mvm_fields = {'trans_x','trans_y','trans_z','rot_x','rot_y','rot_z'};
-rot_IDs = logical([0 0 0 1 1 1]); 
+rot_IDs = logical([0 0 0 1 1 1]);
 
 for i = 1:numdatas
-    
+
     %to account for subject numbers that are all numeric
     if isa(df.sub(i),'double')
         subject = num2str(df.sub(i));
@@ -63,7 +63,7 @@ for i = 1:numdatas
     else
         error('can not recognize subject data type')
     end
-    
+
     % where is the data stored for this particular session
     inputdir = [df.topDir{i} '/' df.dataFolder{i} '/fmriprep/sub-' subject '/ses-' num2str(df.sess(i)) '/func/'];
     %inputdir = [projectdir 'ses-' num2str(ses) '/func/'];
@@ -71,39 +71,39 @@ for i = 1:numdatas
     if ~exist(outputdir)
         mkdir(outputdir);
     end
-    
-    % search for relevant motion files
-    % infiles = dir([inputdir '*' input_filestr]);    
-    % loop through runs in datalist
-    runs = str2double(regexp(df.runs{i},',','split'))'; % get runs, converting to numerical array (other orientiation since that's what's expected
 
-    for r = 1:length(runs)
-        
+    % search for relevant motion files
+    % infiles = dir([inputdir '*' input_filestr]);
+    % loop through runs in datalist
+    %runs = str2double(regexp(df.runs{i},',','split'))'; % get runs, converting to numerical array (other orientiation since that's what's expected
+
+    %for r = 1:length(runs)
+
         % load motion data
         % assume fmriprep data organization
         %confounds = bids.util.tsvread([inputdir infiles(i).name]); %reads into a structure, each field = col in tsv
-%         run_str = sprintf('sub-%s_ses-%d_task-%s_run-%02d',df.sub{i},df.sess(i),df.task{i},runs(r));
-        run_str = sprintf('sub-%s_ses-%d_task-%s_run-%d',subject,df.sess(i),df.task{i},runs(r));
+        %run_str = sprintf('sub-%s_ses-%d_task-%s_run-%02d',df.sub{i},df.sess(i),df.task{i},df.space{i});
+        run_str = sprintf('sub-%s_ses-%d_task-%s_space-%d',subject,df.sess(i),df.task{i},df.space{i});
         confounds = bids.util.tsvread([inputdir run_str '_desc-confounds_timeseries.tsv']); %reads into a structure, each field = col in tsv
-        
+
         % str for naming output (contains subject, task, and run info):
         %run_str = infiles(i).name(1:end-length(input_filestr));
-        
+
         % make a single matrix organized as we want
         mot_data_orig = [];
         for m = 1:length(mvm_fields)
             mot_data_orig = [mot_data_orig confounds.(mvm_fields{m})];
-        end        
-       
+        end
+
         % start by doing some conversions
         % Convert roll, pitch, and yaw to mm (other parameters already in
         % mm). fmriprep = rotation in radians
         mot_data(:,rot_IDs) = mot_data_orig(:,rot_IDs).* headSize ; %.* 2 * pi./360; % rotation params modified
         mot_data(:,~rot_IDs) = mot_data_orig(:,~rot_IDs); % translation params stay the same
-        
+
         % filter mot data
         mot_data_filtered = filter_motion(df.TR(i),mot_data);
-        
+
         %% these lines might need changing throughout the code or txt files might not save appropriately
         %Here are two option: writematrix is more likely to work, but
         %adding -ascii, -double, and -tabs to the save command works for
@@ -112,12 +112,12 @@ for i = 1:numdatas
         writetable(table(mot_data(:,1),mot_data(:,2),mot_data(:,3),mot_data(:,4),mot_data(:,5),mot_data(:,6),...
             'VariableNames',mvm_fields),sprintf('%s%s_desc-mvm.txt',outputdir,run_str));
         %save(sprintf('%s%smvm.txt',outputdir,outstr),'mot_data', '-ascii','-double','-tabs');
-        %writematrix(mot_data_filtered,sprintf('%s%smvm_filt.txt',outputdir,outstr)); 
+        %writematrix(mot_data_filtered,sprintf('%s%smvm_filt.txt',outputdir,outstr));
         writetable(table(mot_data_filtered(:,1),mot_data_filtered(:,2),mot_data_filtered(:,3),...
             mot_data_filtered(:,4),mot_data_filtered(:,5),mot_data_filtered(:,6),...
-                'VariableNames',mvm_fields),sprintf('%s%s_desc-mvm_filt.txt',outputdir,run_str)); 
+                'VariableNames',mvm_fields),sprintf('%s%s_desc-mvm_filt.txt',outputdir,run_str));
         %save(sprintf('%s%smvm_filt.txt',outputdir,outstr),'mot_data_filtered', '-ascii','-double','-tabs');
-        
+
         % calculate FD pre and post filtering
         mot_data_diff = [zeros(1,6); diff(mot_data)];
         mot_data_filt_diff = [zeros(1,6); diff(mot_data_filtered)];
@@ -129,19 +129,19 @@ for i = 1:numdatas
         %writematrix(fFD,sprintf('%s%sfFD.txt',outputdir,outstr));
         writetable(table(fFD),sprintf('%s%s_desc-fFD.txt',outputdir,run_str));
         %save(sprintf('%s%sfFD.txt',outputdir,outstr),'fFD', '-ascii','-double','-tabs');
-        
+
         % plot original parameters & FD
         plot_motion_params(mot_data,FD,FDthresh,mvm_fields);
         print(gcf,sprintf('%s%s_desc-motion_parameters.pdf',outputdir,run_str),'-dpdf','-bestfit');
         plot_motion_params(mot_data_filtered,fFD,fFDthresh,mvm_fields);
         print(gcf,sprintf('%s%s_desc-motion_parameters_filtered.pdf',outputdir,run_str),'-dpdf','-bestfit');
-        
+
         % make some plots - FFT
         mot_FFT(mot_data,df.TR(i),1);
         print(gcf,sprintf('%s%s_desc-motion_FFT.pdf',outputdir,run_str),'-dpdf','-bestfit');
         mot_FFT(mot_data_filtered,df.TR(i),1);
         print(gcf,sprintf('%s%s_desc-motion_filtered_FFT.pdf',outputdir,run_str),'-dpdf','-bestfit');
-        
+
         % make a tmask for each run
         DropFramesTR = df.dropFr(i); %round(DropFramesSec/df.TR(i)); % Calculate num TRs to drop
         tmask_FD = make_tmask(FD,FDthresh,DropFramesTR,contig_frames);
@@ -152,7 +152,7 @@ for i = 1:numdatas
         %writematrix(tmask_fFD,sprintf('%s%stmask_fFD.txt',outputdir,outstr));
         writetable(table(tmask_fFD),sprintf('%s%s_desc-tmask_fFD.txt',outputdir,run_str));
         %save(sprintf('%s%stmask_fFD.txt',outputdir,outstr),'tmask_fFD', '-ascii','-double','-tabs');
-        
+
         % some stats to keep track of
         good_run_FD(r) = sum(tmask_FD) > run_min;
         good_run_fFD(r) = sum(tmask_fFD) > run_min;
@@ -160,11 +160,11 @@ for i = 1:numdatas
         run_frame_nums_fFD(r) = sum(tmask_fFD);
         run_frame_per_FD(r) = sum(tmask_FD)./numel(tmask_FD);
         run_frame_per_fFD(r) = sum(tmask_fFD)./numel(tmask_fFD);
-        
+
         close('all');
-        clear mot_data;        
-    end
-    
+        clear mot_data;
+    %end
+
     % save out some general info
     %writematrix(good_run_FD,sprintf('%sgoodruns_FD.txt',outputdir));
     writetable(table(good_run_FD'),sprintf('%s/sub-%s_ses-%d_task-%s_desc-goodruns_FD.txt',outputdir,subject,df.sess(i),df.task{i}));
@@ -172,21 +172,21 @@ for i = 1:numdatas
     %writematrix(good_run_fFD, sprintf('%sgoodruns_fFD.txt',outputdir));
     writetable(table(good_run_fFD'), sprintf('%s/sub-%s_ses-%d_task-%s_desc-goodruns_fFD.txt',outputdir,subject,df.sess(i),df.task{i}));
     %save(sprintf('%sgoodruns_fFD.txt',outputdir),'good_run_fFD', '-ascii','-double','-tabs');
-    
+
     %writematrix(run_frame_nums_FD,sprintf('%sframenums_FD.txt',outputdir));
     writetable(table(run_frame_nums_FD'),sprintf('%s/sub-%s_ses-%d_task-%s_desc-framenums_FD.txt',outputdir,subject,df.sess(i),df.task{i}));
     %save(sprintf('%sframenums_FD.txt',outputdir),'run_frame_nums_FD', '-ascii','-double','-tabs');
     %writematrix(run_frame_nums_fFD,sprintf('%sframenums_fFD.txt',outputdir));
     writetable(table(run_frame_nums_fFD'),sprintf('%s/sub-%s_ses-%d_task-%s_desc-framenums_fFD.txt',outputdir,subject,df.sess(i),df.task{i}));
     %save(sprintf('%sframenums_fFD.txt',outputdir),'run_frame_nums_fFD', '-ascii','-double','-tabs');
-    
+
     %writematrix(run_frame_per_FD,sprintf('%sframepers_FD.txt',outputdir));
     writetable(table(run_frame_per_FD'),sprintf('%s/sub-%s_ses-%d_task-%s_desc-framepers_FD.txt',outputdir,subject,df.sess(i),df.task{i}));
     %save(sprintf('%sframepers_FD.txt',outputdir),'run_frame_per_FD', '-ascii', '-double', '-tabs');
     %writematrix(run_frame_per_fFD,sprintf('%sframepers_fFD.txt',outputdir));
     writetable(table(run_frame_per_fFD'),sprintf('%s/sub-%s_ses-%d_task-%s_desc-framepers_fFD.txt',outputdir,subject,df.sess(i),df.task{i}));
     %save(sprintf('%sframepers_fFD.txt',outputdir),'run_frame_per_fFD', '-ascii','-double','-tabs');
-    
+
     clear good_run_FD run_frame_nums_FD run_frame_per_FD;
     clear good_run_fFD run_frame_nums_fFD run_frame_per_fFD;
 end
--- scripts/process/fcon_check.sh
+++ scripts/process/fcon_check.sh
@@ -4,4 +4,5 @@
 ### Ellyn Butler
 ### July 28, 2022 - July 31, 2022
 
-matlab /projects/b1108/GrattonLab-General-Repo/motion_calc_utilities/FDcalc_FMRIPREP.m
+matlab /projects/b1108/studies/mwmh/scripts/process/gratton_motioncalc.m \
+  /projects/b1108/studies/mwmh/data/processed/neuroimaging/lists/test_list_for_motioncalc.xlsx
