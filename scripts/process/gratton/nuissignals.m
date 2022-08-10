f04fe9dce9340d99dfc9dddd07aa974ec6382016
Ellyn Butler <ellynrbutler@gmail.com>
2022-08-10 13:35:17 -0500

    nuis one run

--- scripts/process/gratton/nuissignals.m
+++ scripts/process/gratton/nuissignals.m
@@ -15,11 +15,18 @@ for cn = 1:length(conf_names)
 end
 
 % load confounds signals from fmriprep
-for j = 1:length(QC(i).runs)
-    run_confounds = bids.util.tsvread(tboldconf{j});
+if length(QC(i).runs) > 1
+    for j = 1:length(QC(i).runs)
+        run_confounds = bids.util.tsvread(tboldconf{j});
+        for cn = 1:length(conf_names)
+            temprun_confounds=demean_detrend(run_confounds.(conf_names{cn})');        
+            QC(i).(conf_names{cn}) = [QC(i).(conf_names{cn}); temprun_confounds'];
+        end
+    end
+else
+    run_confounds = bids.util.tsvread(tboldconf);
     for cn = 1:length(conf_names)
-        temprun_confounds=demean_detrend(run_confounds.(conf_names{cn})');
-        
+        temprun_confounds=demean_detrend(run_confounds.(conf_names{cn})');           
         QC(i).(conf_names{cn}) = [QC(i).(conf_names{cn}); temprun_confounds'];
     end
 end
