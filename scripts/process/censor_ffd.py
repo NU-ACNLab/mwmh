d4e096eb0470bbdc0a6fdbbcd249d9446c27feaf
Ellyn Butler <ellynrbutler@gmail.com>
2022-10-18 17:04:50 -0500

    60 min

--- scripts/process/censor_ffd.py
+++
@@ -1,7 +0,0 @@
-### Function to censor volumes based on fFD
-### https://nilearn.github.io/dev/auto_examples/03_connectivity/plot_signal_extraction.html#sphx-glr-auto-examples-03-connectivity-plot-signal-extraction-py
-###
-### Ellyn Butler
-### September 16, 2022
-
-def censor_ffd():
--- scripts/process/launch_amygconn.py
+++ scripts/process/launch_amygconn.py
@@ -1,7 +1,7 @@
 ### This script generates submission scripts for fmriprep for the first visit
 ###
 ### Ellyn Butler
-### December 12, 2021
+### December 12, 2021 - October 18, 2022
 
 
 import os
@@ -10,26 +10,27 @@ import re
 import numpy as np
 import glob
 
-indir = '/projects/b1108/data/MWMH/fmriprep/'
-outdir = '/projects/b1108/data/MWMH/amygconn/'
-launchdir = '/projects/b1108/data/MWMH/launch/amygconn/'
+indir = '/projects/b1108/studies/mwmh/data/processed/neuroimaging/fmriprep/'
+outdir = '/projects/b1108/studies/mwmh/data/processed/neuroimaging/amygconn/'
+bidsdir = '/projects/b1108/studies/mwmh/data/raw/neuroimaging/bids/'
+launchdir = '/projects/b1108/studies/mwmh/data/processed/neuroimaging/launch/amygconn/'
 
-subDirs = glob.glob(indir + "sub-*[!.html]")
+subdirs = glob.glob(indir + "sub-*[!.html]")
 
-for subDir in subDirs:
-    sub = subDir.split('/')[6]
+for subdir in subdirs:
+    sub = subdir.split('/')[9]
     if not os.path.exists(outdir+sub):
         os.mkdir(outdir+sub)
     bold_imgs = glob.glob(indir+sub+'/*/*/*bold.nii.gz')
     if len(bold_imgs) > 0:
-        sessions = [i.split('/')[7] for i in bold_imgs]
+        sessions = np.unique([i.split('/')[10] for i in bold_imgs])
         for ses in sessions:
             if not os.path.exists(outdir+sub+'/'+ses):
                 os.mkdir(outdir+sub+'/'+ses)
-            cmd = ['python3 /home/erb9722/studies/mwmh/scripts/process/amygconn.py -i',
-                indir, '-o', outdir, '-s', sub, '-ss', ses]
+            cmd = ['python3 /projects/b1108/studies/mwmh/scripts/process/amygconn.py -i',
+                indir, '-o', outdir, '-b', bidsdir, '-s', sub, '-ss', ses]
             amygconn_script = launchdir+sub+'_'+ses+'_amygconn_run.sh'
-            os.system('cat /home/erb9722/studies/mwmh/scripts/process/sbatchinfo_10min_general.sh > '+amygconn_script)
+            os.system('cat /projects/b1108/studies/mwmh/scripts/process/sbatchinfo_60min_general.sh > '+amygconn_script)
             os.system('echo '+' '.join(cmd)+' >> '+amygconn_script)
             os.system('chmod +x '+amygconn_script)
-            os.system('sbatch -o /projects/b1108/data/MWMH/launch/amygconn/'+sub+'_'+ses+'.txt'+' '+amygconn_script)
+            os.system('sbatch -o '+launchdir+sub+'_'+ses+'.txt'+' '+amygconn_script)
