c4948dcd6d83155d7f14eefde788fc7ad8835762
Ellyn Butler <ellynrbutler@gmail.com>
2022-10-19 10:01:06 -0500

    try more mem

--- scripts/process/sbatchinfo_60min_general.sh
+++ scripts/process/sbatchinfo_60min_general.sh
@@ -4,7 +4,7 @@
 #SBATCH --nodes=1                                         ## how many computers do you need
 #SBATCH --ntasks-per-node=1                               ## how many cpus or processors do you need on each computer
 #SBATCH --time=00:60:00                                   ## how long does this need to run (remember different partitions have restrictions on this param)
-#SBATCH --mem-per-cpu=40G                                 ## how much RAM do you need per CPU (this effects your FairShare score so be careful to not ask for more than you need))
+#SBATCH --mem-per-cpu=100G                                ## how much RAM do you need per CPU (this effects your FairShare score so be careful to not ask for more than you need))
 #SBATCH --job-name="min60"                                ## use the task id in the name of the job
 #SBATCH --mail-type=FAIL                                  ## you can receive e-mail alerts from SLURM when your job begins and when your job finishes (completed, failed, etc)
 #SBATCH --mail-user=ellynbutler2027@u.northwestern.edu    ## your email
