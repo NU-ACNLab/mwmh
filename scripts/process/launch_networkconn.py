c91e61bbe1ce385d4bd61d8293a742d83efa6fe8
Ellyn Butler <ellynrbutler@gmail.com>
2023-06-29 22:52:28 -0500

    low mem job

--- scripts/process/launch_networkconn.py
+++ scripts/process/launch_networkconn.py
@@ -20,34 +20,17 @@ for subdir in subdirs:
     sub = subdir.split('/')[9]
     if not os.path.exists(outdir+sub):
         os.mkdir(outdir+sub)
-    sub_bold_imgs = glob.glob(indir+sub+'/*/*/*bold.nii.gz')
-    sessions = np.unique([i.split('/')[10] for i in sub_bold_imgs])
+    amygcorrs = glob.glob(indir+sub+'/*/*_amygcorr.csv')
+    sessions = np.unique([i.split('/')[10] for i in amygcorrs])
     for ses in sessions:
         if not os.path.exists(outdir+sub+'/'+ses):
             os.mkdir(outdir+sub+'/'+ses)
-        ses_bold_imgs = glob.glob(indir+sub+'/'+ses+'/*/*bold.nii.gz')
-        if len(ses_bold_imgs) > 0:
-            tasks_list = np.unique([i.split('/')[12].split('_')[2].split('-')[1] for i in ses_bold_imgs])
-            ses_events_files = glob.glob(bidsdir+sub+'/'+ses+'/*/*events.tsv')
-            events_list = np.unique([i.split('/')[12].split('_')[2].split('-')[1] for i in ses_events_files])
-            # If you don't have an events file, you can't include the task fMRI
-            if 'avoid' in tasks_list and 'avoid' not in events_list:
-                index = np.argwhere(tasks_list == 'avoid')
-                tasks_list = np.delete(tasks_list, index)
-            if 'faces' in tasks_list and 'faces' not in events_list:
-                index = np.argwhere(tasks_list == 'faces')
-                tasks_list = np.delete(tasks_list, index)
-            # If the rest scan is too short, you can't process it
-            rest_img = nib.load(indir+sub+'/'+ses+'/func/'+sub+'_'+ses+'_task-rest_space-MNI152NLin6Asym_desc-preproc_bold.nii.gz')
-            if rest_img.shape[3] < 34:
-                index = np.argwhere(tasks_list == 'rest')
-                tasks_list = np.delete(tasks_list, index)
-            if len(tasks_list) > 0:
-                tasks = ' '.join(tasks_list)
-                cmd = ['python3 /projects/b1108/studies/mwmh/scripts/process/amygconn.py -i',
-                    indir, '-o', outdir, '-b', bidsdir, '-s', sub, '-ss', ses, '-t', tasks]
-                amygconn_script = launchdir+sub+'_'+ses+'_amygconn_run.sh'
-                os.system('cat /projects/b1108/studies/mwmh/scripts/process/sbatchinfo_40min_general.sh > '+amygconn_script)
-                os.system('echo '+' '.join(cmd)+' >> '+amygconn_script)
-                os.system('chmod +x '+amygconn_script)
-                os.system('sbatch -o '+launchdir+sub+'_'+ses+'.txt'+' '+amygconn_script)
+        ses_corrmats = glob.glob(indir+sub+'/'+ses+'/*_corrmat.csv')
+        if len(ses_corrmats) > 0:
+            cmd = ['python3 /projects/b1108/studies/mwmh/scripts/process/networkconn.py -i',
+                    indir, '-o', outdir, '-s', sub, '-ss', ses]
+            networkconn_script = launchdir+sub+'_'+ses+'_networkconn_run.sh'
+            os.system('cat /projects/b1108/studies/mwmh/scripts/process/sbatchinfo_40min_general.sh > '+amygconn_script)
+            os.system('echo '+' '.join(cmd)+' >> '+networkconn_script)
+            os.system('chmod +x '+networkconn_script)
+            os.system('sbatch -o '+launchdir+sub+'_'+ses+'.txt'+' '+networkconn_script)
