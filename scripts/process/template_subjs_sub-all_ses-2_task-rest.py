e8d3e0decbb3a9c3cf2d46ed9c958f57163d3e98
Ellyn Butler <ellynrbutler@gmail.com>
2024-06-07 11:07:54 -0500

    try all the session 2 subjects in a template

---
+++ scripts/process/template_subjs_sub-all_ses-2_task-rest.py
@@ -0,0 +1,37 @@
+### This script identifies individuals who only have one session
+### of resting state data so that they can be used in template
+### construction, without losing any subjects from the ultimate
+### analysis (which requires two session per participant)
+### 
+### Ellyn Butler
+### June 7, 2024
+
+import os
+import shutil
+import re
+import numpy as np
+import glob
+import pandas as pd
+
+indir = '/projects/b1108/studies/mwmh/data/processed/neuroimaging/surf/'
+outdir = '/projects/b1108/studies/mwmh/data/processed/neuroimaging/tabulated/'
+
+subdirs = glob.glob(indir + "sub-*")
+
+temp_subjs = {'subid':[], 'sesid':[]}
+
+for subdir in subdirs:
+    sub = subdir.split('/')[9]
+    subid = sub.split('-')[1]
+    if subid == 'MWMH001':
+        continue
+    ses2_rest = os.path.exists(indir + sub + '/ses-2/func/' + sub + '_ses-2_task-rest_space-fsLR_desc-postproc_bold.dscalar.nii')
+    ses2_faces = os.path.exists(indir + sub + '/ses-2/func/' + sub + '_ses-2_task-faces_space-fsLR_desc-postproc_bold.dscalar.nii')
+    ses2_avoid = os.path.exists(indir + sub + '/ses-2/func/' + sub + '_ses-2_task-avoid_space-fsLR_desc-postproc_bold.dscalar.nii')
+    if ses2_rest or ses2_faces or ses2_avoid:
+        sesid = 2
+        temp_subjs['subid'].append(subid)
+        temp_subjs['sesid'].append(sesid)
+
+temp_subjs_df = pd.DataFrame(temp_subjs)
+temp_subjs_df.to_csv(outdir + 'temp_subjs_sub-all_ses-2_task-all.csv', index = False)
