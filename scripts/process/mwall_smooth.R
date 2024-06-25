8543f0b8f367a690e51f4ee4f6d97050a37ba4f6
Ellyn Butler <ellynrbutler@gmail.com>
2024-06-25 17:09:17 -0500

    need the yeo mwall

--- scripts/process/mwall_smooth.R
+++ scripts/process/mwall_smooth.R
@@ -36,6 +36,54 @@ print(sesid)
 
 Sys.setenv('R_MAX_VSIZE'=32000000000)
 
+# ---------- Load and modify the Yeo 17 parcellation ---------- #
+GPARC <- ciftiTools::load_parc('Yeo_17')
+### Get new labels by removing the subnetwork and LH/RH parts of the label.
+y <- rownames(GPARC$meta$cifti$labels[[1]])
+z <- gsub('17Networks_LH_|17Networks_RH_', '', y)
+z <- gsub('_.*', '', z)
+# ### Uncomment the below two lines if you want to keep left and right separate.
+# y <- gsub('17Networks_', '', gsub('H_.*', 'H_', gsub('???', '', y)))
+# z <- paste0(y, z)
+### Apply new labels.
+z <- factor(z, levels=z[!duplicated(z)])
+GPARC <- convert_to_dlabel(
+  newdata_xifti(GPARC, as.numeric(z)[c(as.matrix(GPARC))+1]),
+  levels_old=as.numeric(z)[!duplicated(z)],
+  levels=as.numeric(z)[!duplicated(z)] - 1,
+  labels=levels(z),
+  colors=rgb(
+    GPARC$meta$cifti$labels[[1]]$Red,
+    GPARC$meta$cifti$labels[[1]]$Green,
+    GPARC$meta$cifti$labels[[1]]$Blue,
+    GPARC$meta$cifti$labels[[1]]$Alpha
+  )[!duplicated(z)],
+  add_white=FALSE
+)
+GPARC$meta$cifti$labels[[1]] <- GPARC$meta$cifti$labels[[1]]
+### Handle medial wall
+GPARC$meta$cifti$labels[[1]] <- rbind(
+  data.frame(Key=-1, Red=1, Green=1, Blue=1, Alpha=0, row.names='BOLD_mwall'),
+  GPARC$meta$cifti$labels[[1]]
+)
+
+mwall_path <- paste0(hcp_dir, 'Human.MedialWall_Conte69.32k_fs_LR.dlabel.nii')
+mwall_cifti <- read_cifti(mwall_path)
+
+mwall_L <- c(mwall_cifti$data$cortex_left)
+mwall_L <- recode(mwall_L, `0`=TRUE, `1`=FALSE)
+mwall_R <- c(mwall_cifti$data$cortex_right)
+mwall_R <- recode(mwall_R, `0`=TRUE, `1`=FALSE)
+
+GPARC$data$cortex_left[!mwall_L,] <- NA
+GPARC$data$cortex_right[!mwall_R,] <- NA
+
+GPARC <- move_to_mwall(GPARC, values = c(NA, 0)) #why not NA?
+
+mwall_L <- GPARC$meta$cortex$medial_wall$left
+mwall_R <- GPARC$meta$cortex$medial_wall$right
+
+# --------------- Modify the subject cifti --------------- #
 paths <- c(system(paste0('find ', indir, 'surf/sub-', subid, '/ses-', 
         sesid, '/func/ ', '-name "*_space-fsLR_desc-postproc_bold.dscalar.nii"'), intern=TRUE))
 i = 1
